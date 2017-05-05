90a1431869fbc48c96f8989a0b1383fb95473942
Cliffs Dover <cliffs@dancingbottle.com>
2017-05-05 09:26:13 +0800

    Add iOS project

---
+++ OpenRCT2_iOS/OpenRCT2_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenRCT2_iOS
+//
+//  Created by MyStaub on 2017/5/5.
+//  Copyright © 2017年 Dancing Bottle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenRCT2_iOS/OpenRCT2_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenRCT2_iOS
+//
+//  Created by MyStaub on 2017/5/5.
+//  Copyright © 2017年 Dancing Bottle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
